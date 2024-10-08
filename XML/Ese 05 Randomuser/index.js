7b1b4b1087ce4cc3a0ea411508344d21a87da1f0
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2024-10-08 15:49:20 +0200

    first commit: aggiunta primi 5 file XML

---
+++ XML/Ese 05 Randomuser/index.js
@@ -0,0 +1,93 @@
+"use strict"
+
+const headers = ["Name", "City", "State", "Nat", "", ""];
+const headersWidth = ["25%", "25%", "25%", "10%", "7.5%", "7.5%"];
+let currentPage = 0;
+let gender = 0;
+
+window.onload=function()
+{
+	const opts = document.querySelectorAll("#optWrapper input");
+	const table = document.querySelector("table");
+	const btns = document.querySelectorAll("#buttons input");
+	const details = document.getElementById("details");
+	const nPag = document.getElementById("nPagina");
+	const tbody = document.createElement("tbody");
+
+	let xml = localStorage.getItem("people_xml");
+	if(!xml)
+	{
+		xml = people;
+	}
+	let parser = new DOMParser();
+	let xmlDOC = parser.parseFromString(xml, "text/xml");
+	let xmlRoot = xmlDOC.firstElementChild;
+
+	caricaIntestazione();
+	caricaTabella();
+
+	function caricaIntestazione(){
+		const thead = document.createElement("thead");
+		table.appendChild(thead);
+		const tr = document.createElement("tr");
+		thead.appendChild(tr);
+
+		headers.forEach(function(header, i){
+			const th = document.createElement("th");
+			tr.appendChild(th);
+			th.textContent = header;
+			th.style.width = headersWidth[i];
+		});
+		table.appendChild(tbody);
+	}
+	
+	function caricaTabella(){
+		tbody.innerHTML = "";
+
+		let firstRecord = currentPage * 6;
+
+		let array = Array.from(xmlRoot.children[gender].children);
+
+		for (let i = firstRecord; i < (firstRecord + 6) && (i < array.length); i++) {
+			const person = xmlRoot.children[gender].children[i];
+			
+			const tr = document.createElement("tr");
+			tbody.appendChild(tr);
+
+			let td = document.createElement("td");
+			tr.appendChild(td);
+			td.textContent = person.querySelector("name").querySelector("first").textContent + " " + person.querySelector("name").querySelector("last").textContent;
+
+			td = document.createElement("td");
+			tr.appendChild(td);
+			td.textContent = person.querySelector("location").querySelector("city").textContent;
+
+			td = document.createElement("td");
+			tr.appendChild(td);
+			td.textContent = person.querySelector("location").querySelector("state").textContent;
+
+			td = document.createElement("td");
+			tr.appendChild(td);
+			td.textContent = person.lastElementChild.textContent;
+
+			td = document.createElement("td");
+			tr.appendChild(td);
+			td.style.backgroundImage = 'url("./img/lente.jpg")';
+			td.style.backgroundSize = "contain";
+			td.style.backgroundRepeat = "no-repeat";
+			td.style.padding = "5px";
+			td.style.backgroundOrigin = "content-box" //serve a far sì che l'immagine non si espandi nel padding
+			
+
+			td = document.createElement("td");
+			tr.appendChild(td);
+			td.style.backgroundImage = 'url("./img/delete.png")';
+			td.style.backgroundSize = "contain";
+			td.style.backgroundRepeat = "no-repeat";
+			td.style.padding = "5px";
+			td.style.backgroundOrigin = "content-box" //serve a far sì che l'immagine non si espandi nel padding
+			
+		}
+	}
+
+} 
