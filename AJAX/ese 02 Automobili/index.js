16dd1bfb6f501efed5dd9b97709b8089f6442f2f
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2025-01-15 08:55:32 +0100

    Es info IComparer

--- AJAX/ese 02 Automobili/index.js
+++ AJAX/ese 02 Automobili/index.js
@@ -2,17 +2,68 @@
 
 $(document).ready(function () {
 	
-	let _lstMarche = $("#lstMarche");
-	let _lstModelli = $("#lstModelli");
-	let _table= $("table")
-	let _dettagli=$(".details")
+	let _lstMarche = document.getElementById("lstMarche");
+	let _lstModelli = document.getElementById("lstModelli");
+	let _table= document.getElementsByTagName("table")[0];
+	let _dettagli = document.querySelector(".details");
 
-	_table.hide()
-	_dettagli.hide()
+	$(_table).hide()
+	$(_dettagli).hide()
 
-       
+	getMarche();
 
+	function getMarche(){
+		let request = inviaRichiesta("GET", "/marche");
+
+		request.catch(errore);
+		request.then(function(response){
+			let marche = response.data;
+			console.log(marche);
+
+			for (const marca of marche) {
+				let opt = document.createElement("option");
+				_lstMarche.appendChild(opt);
+				opt.value = marca.id;
+				opt.textContent = marca.nome;
+			}
+
+			_lstMarche.selectedIndex = -1;
+		})
+	}
+
+	_lstMarche.addEventListener("change", function(){
+		const codMarca = this.value;
 		
+		let request = inviaRichiesta("GET", "/modelli", {"codMarca": codMarca});
+
+		request.catch(errore);
+		request.then(function(response){
+			let modelli = response.data;
+
+			_lstModelli.innerHTML = "";
+
+			for(const modello of modelli){
+				let opt = document.createElement("option");
+				_lstModelli.appendChild(opt);
+				opt.value = modello.id;
+				opt.textContent = modello.nome + " - " + modello.alimentazione;
+			}
+
+			_lstModelli.selectedIndex = -1;
+		})
+	})
+
+	_lstModelli.addEventListener("change", function(){
+		let codModello = this.value;
+
+		let request = inviaRichiesta("GET", "/automobili", {"codModello": codModello});
+
+		request.catch(errore);
+		request.then(function(response){
+			
+		})
+	})
+
 });
 
 
---
+++ InterfacciaOrdinamentoPiuCampi/InterfacciaOrdinamentoPiuCampi/Persona.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacciaOrdinamentoPiuCampi
+{
+    internal class Persona
+    {
+        public string nome;
+        public string citta;
+
+        public Persona(string nome, string citta)
+        {
+            this.nome = nome;
+            this.citta = citta;
+        }
+
+        public class confrontaNome : IComparer<Persona> 
+        {
+            public int Compare(Persona p1, Persona p2) 
+            {
+                if (p1 == null && p2 == null) return 0;
+                else if (p1 == null) return 1;
+                else if (p2 == null) return -1;
+                else
+                {
+                    return string.Compare(p1.nome, p2.nome, true);
+                }
+            }
+        }
+
+        public class confrontaCitta : IComparer<Persona>
+        {
+            public int Compare(Persona p1, Persona p2)
+            {
+                if (p1 == null && p2 == null) return 0;
+                else if (p1 == null) return 1;
+                else if (p2 == null) return -1;
+                else
+                {
+                    return string.Compare(p1.citta, p2.citta, true);
+                }
+            }
+        }
+    }
+}
