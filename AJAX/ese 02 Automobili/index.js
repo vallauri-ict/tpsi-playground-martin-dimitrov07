fea67e9935a85cbffd1c4b7c7486730c3f2c5a4a
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2025-01-17 10:50:02 +0100

    Fine es 2 AJAX

--- AJAX/ese 02 Automobili/index.js
+++ AJAX/ese 02 Automobili/index.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
 	let _lstModelli = document.getElementById("lstModelli");
 	let _table= document.getElementsByTagName("table")[0];
 	let _dettagli = document.querySelector(".details");
+	let _tbody = _table.lastElementChild;
+	let _btnUpdatePrice = document.getElementById("btnAggiorna");
 
 	$(_table).hide()
 	$(_dettagli).hide()
@@ -31,9 +33,12 @@ $(document).ready(function () {
 		})
 	}
 
-	_lstMarche.addEventListener("change", function(){
+	_lstMarche.addEventListener("change", function(){ 
 		const codMarca = this.value;
-		
+		getModelli(codMarca); 
+	});
+
+	function getModelli(codMarca){		
 		let request = inviaRichiesta("GET", "/modelli", {"codMarca": codMarca});
 
 		request.catch(errore);
@@ -51,19 +56,132 @@ $(document).ready(function () {
 
 			_lstModelli.selectedIndex = -1;
 		})
-	})
+	}
 
 	_lstModelli.addEventListener("change", function(){
-		let codModello = this.value;
+		const codModello = this.value;
+		//const selectedText = this.options[this.selectedIndex].textContent;
+		const selectedText = this.selectedOptions[0].textContent;
+		getCars(codModello, selectedText)
+	})
+
+	function getCars(codModello, selectedText){
+		const aus = selectedText.split(" - ");
+		const nomeModello = aus[0];
+		const tipoAlimentazione = aus[1];
 
 		let request = inviaRichiesta("GET", "/automobili", {"codModello": codModello});
 
 		request.catch(errore);
 		request.then(function(response){
-			
-		})
-	})
+			const automobili = response.data;
 
+			_tbody.innerHTML = "";
+			$(_table).show();
+
+			for (const auto of automobili) {
+				const tr = document.createElement("tr");
+				_tbody.appendChild(tr);
+
+				let td = document.createElement("td");
+				tr.appendChild(td);
+				td.textContent = nomeModello;
+
+				td = document.createElement("td");
+				tr.appendChild(td);
+				td.textContent = tipoAlimentazione;
+
+				td = document.createElement("td");
+				tr.appendChild(td);
+				td.textContent = auto.colore;
+
+				td = document.createElement("td");
+				tr.appendChild(td);
+				td.textContent = auto.anno;
+
+				td = document.createElement("td");
+				tr.appendChild(td);
+				const img = document.createElement("img");
+				img.src = "./img/" + auto.img;
+				img.style.height = "65px";
+				td.appendChild(img);
+
+				td = document.createElement("td");
+				tr.appendChild(td);
+				let btn = document.createElement("button");
+				btn.classList.add("btn", "btn-success", "btn-sm");
+				btn.addEventListener("click", function(){ getDetails(auto); })
+				btn.textContent = "dettagli";
+				td.appendChild(btn);
+
+				td = document.createElement("td");
+				tr.appendChild(td);
+				btn = document.createElement("button");
+				btn.classList.add("btn", "btn-secondary", "btn-sm");
+				btn.textContent = "elimina";
+				btn.addEventListener("click", function(){
+					elimina(auto.id);
+				})
+				td.appendChild(btn);
+			}
+		})
+	}
+
+	function getDetails(auto){
+		let request = inviaRichiesta("GET", "/modelli/" + auto.codModello); //restituisce record
+		//let request = inviaRichiesta("GET", "/modelli", { "id": auto.codModello }); restituisce vettore di record
+		$(_dettagli).show();
+
+		request.catch(errore);
+
+		request.then(function(response){
+			const modello = response.data;
+
+			let txts = _dettagli.querySelectorAll("input[type=text]");
+			txts[0].value = auto.id;
+			txts[1].value = modello.nome;
+			txts[2].value = modello.alimentazione;
+			txts[3].value = modello.cilindrata;
+			txts[4].value = auto.targa;
+			txts[5].value = auto.colore;
+			txts[6].value = auto.anno;
+			txts[7].value = auto.km;
+			txts[8].value = auto.prezzo;
+			const img = document.getElementById("imgLarge");
+			img.src = "./img/" + auto.img;
+			img.style.height = "100px";
+		})
+
+	}
+
+	function elimina(idAuto)
+	{
+		//DELETE
+		let request = inviaRichiesta("DELETE", "/automobili/" + idAuto); //id passato solo in coda alla risorsa
+
+		request.catch(errore);
+		request.then(function(response){
+			console.log(response.data);
+			getCars(_lstModelli.value,  _lstModelli.selectedOptions[0].textContent);
+			alert("Automobile eliminato correttamente");
+		})
+	}
+
+	_btnUpdatePrice.addEventListener("click", function(){
+		let idAuto = document.getElementById("txtId").value;
+		let prezzo = parseInt(document.getElementById("txtPrezzo").value);
+		
+		//let request = inviaRichiesta("PATCH", "/automobili/" + idAuto, { "prezzo": prezzo });
+		let request = inviaRichiesta("PATCH", "/automobili/" + idAuto, { prezzo }); //crea una chiave JSON che si chiama prezzo che ha come contenuto il valore della variabile prezzo (se la chiave c'è già la sovrascrive)
+
+		request.catch(errore);
+		request.then(function(response){
+			alert("Prezzo dell'automobile modificato correttamente");
+			$(_dettagli).hide();
+			getCars(_lstModelli.value, _lstModelli.selectedOptions[0].textContent);
+		})
+		
+	})
 });
 
 
