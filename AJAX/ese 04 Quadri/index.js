46e131677653fbad58c46e08f4b2aa0dc81d7767
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2025-01-29 09:45:17 +0100

    Avanzamento Es 04 Quadri AJAX (Fino all'immagine Base64)

--- AJAX/ese 04 Quadri/index.js
+++ AJAX/ese 04 Quadri/index.js
@@ -1,5 +1,9 @@
 "use strict"
 
+let quadri = [];
+let checkedArtist;
+let indexPainting = 0;
+
 $(function () {
     let _head = $('.head')[0];
     let _info = $('.info')[0];
@@ -7,10 +11,6 @@ $(function () {
     let _btnPrev = $('button')[0];
     let _btnNext = $('button')[1];
 	let _wrapperAdd = $('.wrapper')[1];
-
-    let quadri = [];
-    let checkedArtist;
-	let indexPainting = 0;
 
     _btnPrev.disabled = true;
 	
@@ -41,20 +41,24 @@ $(function () {
             const input = document.createElement("input");
             input.type = "radio";
             input.name = "artisti";
-            input.setAttribute("artista", JSON.stringify(artist)); //solo stringhe quindi convertire in stringa il JSON
-            lbl.addEventListener("click", function(){
+            input.setAttribute("data-artista", JSON.stringify(artist)); //solo stringhe quindi convertire in stringa il JSON
+            input.addEventListener("click", function(){
                 indexPainting = 0;
                 statusBtns();
-                checkedArtist = JSON.parse(this.querySelector("input").getAttribute("artista"));
+                checkedArtist = JSON.parse(this.getAttribute("data-artista"));
                 loadPaintings(checkedArtist);
             })
             lbl.appendChild(input);
-            lbl.innerHTML += artist.name;
+            //riscrive il contenuto dell'oggetto label e quindi gli oggetti interni perdono sia gli eventi sia gli attributi personalizzati
+            // lbl.innerHTML += artista.name;
+            const span = document.createElement("span");
+            span.textContent = artist.name;
+            lbl.appendChild(span);
         }
         
         let nArtista = random(0, artists.length);
 
-        checkedArtist = JSON.parse(_head.querySelectorAll("input[type=radio]")[nArtista].getAttribute("artista"));
+        checkedArtist = JSON.parse(_head.querySelectorAll("input[type=radio]")[nArtista].getAttribute("data-artista"));
         //console.log(checkedArtist);
         _head.querySelectorAll("input[type=radio]")[nArtista].checked = true;
 
@@ -63,9 +67,7 @@ $(function () {
 
     async function loadPaintings(artista)
     {
-        console.log(artista);
-
-        quadri = [];
+        //console.log(artista);
 
         let response = await inviaRichiesta("GET", "/quadri", { "artist": artista.id }).catch(errore);
 
@@ -73,50 +75,55 @@ $(function () {
 
         //console.log(quadri);
 
-        showPaintings(quadri[indexPainting]);
+        showPaintings();
     }
 
-    async function showPaintings(quadro)
+    async function showPaintings()
     {
-        //console.log(quadro)
-
-        let response = await inviaRichiesta("GET", "/artisti/" + quadro.artist).catch(errore);
-        let artist = response.data;
+        let quadro = quadri[indexPainting];
 
         _info.innerHTML = "";
         _img.innerHTML = "";
         
         //INFO
         let div = document.createElement("div");
-        div.textContent = `ID = ${quadro.id}`;
+        div.innerHTML = `ID = <b>${quadro.id}</b>`;
         _info.appendChild(div);
 
         div = document.createElement("div");
-        div.textContent = `Titolo = ${quadro.title}`;
+        div.innerHTML = `Titolo = <b>${quadro.title}</b>`;
         _info.appendChild(div);
 
         div = document.createElement("div");
-        div.textContent = `Genere = ${artist.gender}`;
+        div.innerHTML = `Genere = <b>${checkedArtist.gender}</b>`;
         _info.appendChild(div);
 
         div = document.createElement("div");
-        div.textContent = `Like = ${quadro.nLike}`;
+        div.innerHTML = `Like = <b>${quadro.nLike}</b>`;
+        const imgLike = document.createElement("img");
+        imgLike.src = "./img/like.jpg";
+        imgLike.classList.add("like");
+        div.appendChild(imgLike);
         _info.appendChild(div);
 
         //IMG
         const img = document.createElement("img");
-        img.src = `./img/${quadro.img}`;
+        //se l'immagine non è base64
+        if(!quadro.img.startsWith("data:image/"))
+            img.src = `./img/${quadro.img}`;
+        else //se l'immagine è base64
+            img.src = quadro.img;
         _img.appendChild(img);
     }
 
     function statusBtns()
     {
-        if(indexPainting == 0)
+        if(indexPainting <= 0)
             _btnPrev.disabled = true;
         else
             _btnPrev.disabled = false;
         
-        if(indexPainting == quadri.length - 1)
+        if(indexPainting >= quadri.length - 1)
             _btnNext.disabled = true;
         else
             _btnNext.disabled = false;
