22a925315e97ec779e5912db340e4fad1e06717f
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2025-02-12 22:23:19 +0100

    Iniziato Es 08 AJAX Librerie SweetAlert e Grafici

---
+++ AJAX/ese 08 SweetAlert and Chart/index.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+$(document).ready(function(){		
+	const wrapper = $("#wrapper")[0]
+	const table = $("table")[0]
+	const canvasContainer = $("#canvas")[0] 
+	const canvas = $("canvas")[0]
+
+	$("a").hide();
+	$(table).hide();
+	$(canvasContainer).hide()
+	
+	let chart;
+	
+	btnInvia.addEventListener("click", async () => {
+		//result (response) che in questo caso è un numero contenente la scelta effettuata (quanti utenti) 
+		let result = await Swal.fire({
+			"title": "<b>How many people?</b>",
+			"icon": "question",
+			"input": "range",
+			"inputLabel": "Inserire un numero tra 1 e 100",
+			"inputAttributes": {
+				"min": 1,
+				"max": 100,
+				"step": 1
+			},
+			"inputValue": 100,
+			"width": "400px",
+			"background": "#cfc",
+			"showCancelButton": true
+		})
+	})
+
+})
